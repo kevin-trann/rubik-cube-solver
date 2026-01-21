57696a8554821ff81a3e091800f1e6d53cea11cd
kevin <trankevin789@gmail.com>
2026-01-21 08:04:32 -0500

    Update main.py
    -added temp numbers to pieces to help with debugging and testing
    -added r() move
    -added printCube function to help with debugging and testing

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 cube = (
         [['Y1', 'Y2', 'Y3'], ['Y4', 'Y5', 'Y6'], ['Y7', 'Y8', 'Y9']], #top
-        [['B', 'B', 'B'], ['B', 'B', 'B'], ['B', 'B', 'B']], #front
-        [['R', 'R', 'R'], ['R', 'R', 'R'], ['R', 'R', 'R']], #right
-        [['G', 'G', 'G'], ['G', 'G', 'G'], ['G', 'G', 'G']], #back
-        [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']], #left
-        [['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', 'W']], #bottom
+        [['B1', 'B2', 'B3'], ['B4', 'B5', 'B6'], ['B7', 'B8', 'B9']], #front
+        [['R1', 'R2', 'R3'], ['R4', 'R5', 'R6'], ['R7', 'R8', 'R9']], #right
+        [['G1', 'G2', 'G3'], ['G4', 'G5', 'G6'], ['G7', 'G8', 'G9']], #back
+        [['O1', 'O2', 'O3'], ['O4', 'O5', 'O6'], ['O7', 'O8', 'O9']], #left
+        [['W1', 'W2', 'W3'], ['W4', 'W5', 'W6'], ['W7', 'W8', 'W9']], #bottom
         )
 
 #Moves
 
 def u():
     
-    temp = cube[1][0] #temp to hold top blue row
+    temp = cube[1][0].copy() #temp to hold top blue row
     cube[1][0] = cube[2][0] #change top blue row to top red row
     cube[2][0] = cube[3][0] #change top red row to top green row
     cube[3][0] = cube[4][0] #change top green row to top orange row
@@ -26,11 +26,8 @@ def u():
     cube[0][1] = newMiddleRow
     cube[0][2] = newBottomRow
    
-    
-    
-
 def uPrime():
-    temp = cube[1][0] #temp to hold top blue row
+    temp = cube[1][0].copy() #temp to hold top blue row
     cube[1][0] = cube[4][0] #change top blue row to top orange row
     cube[4][0] = cube[3][0] #change top orange row to top green row
     cube[3][0] = cube[2][0] #change top green row to top red row
@@ -46,5 +43,46 @@ def uPrime():
     cube[0][1] = newMiddleRow
     cube[0][2] = newBottomRow
     
-uPrime()
-print(cube) 
+def r():
+    temp = [ cube[1][0][2], cube[1][1][2], cube[1][2][2]]  #hold right blue column
+
+    #change blue right column to white right column
+    cube[1][0][2] = cube[5][0][2]
+    cube[1][1][2] = cube[5][1][2]
+    cube[1][2][2] = cube[5][2][2]
+    
+    #change white right column to green left column
+    cube[5][0][2] = cube[3][2][0]
+    cube[5][1][2] = cube[3][1][0]
+    cube[5][2][2] = cube[3][0][0]
+    
+    #change green left column to yellow right column
+    cube[3][0][0] = cube[0][2][2]
+    cube[3][1][0] = cube[0][1][2]
+    cube[3][2][0] = cube[0][0][2]
+    
+    #change yellow right column to blue right column    
+    for i in range(3):
+         cube[0][i][2] = temp[i]
+         
+    #changing right pieces
+    # 7, 4, 1 : 8, 5, 2 : 9, 6, 3
+    newTopRow = [ cube[2][2][0], cube[2][1][0], cube[2][0][0]]
+    newMiddleRow = [ cube[2][2][1], cube[2][1][1], cube[2][0][1]]
+    newBottomRow = [ cube[2][2][2], cube[2][1][2], cube[2][0][2]]
+    
+    cube[2][0] = newTopRow 
+    cube[2][1] = newMiddleRow
+    cube[2][2] = newBottomRow
+
+def printCube():
+    print(f"Top: {cube[0]}")
+    print(f"Front: {cube[1]}")
+    print(f"Right: {cube[2]}")
+    print(f"Back: {cube[3]}")
+    print(f"Left: {cube[4]}")
+    print(f"Bottom: {cube[5]}")
+    
+    
+r()
+printCube()
